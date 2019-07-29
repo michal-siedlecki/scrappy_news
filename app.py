4d89f1d79db9f20bb0a297edc70e738102244dad
Michal Siedlecki <siedlecki.michal@gmail.com>
2019-07-29 17:15:19 +0200

    new sites

--- app.py
+++ app.py
@@ -3,33 +3,43 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
+news_num = 6
 
 def onet_list():
     page = requests.get("https://wiadomosci.onet.pl/")
     soup = BeautifulSoup(page.content, 'html.parser')
     news = soup.find_all(class_ = "driverItemTitle itemAnimation")
-    return [x.next for x  in news[1:5]]
-
+    return [x.next for x  in news[1:(news_num-1)]]
 
 def wpolityce_list():
     page = requests.get("https://wpolityce.pl/")
     soup = BeautifulSoup(page.content, 'html.parser')
     news = soup.find_all(class_ = "short-title")
-    return [x.text for x in news[:4]]
-
+    return [x.text for x in news[:news_num]]
 
 def wp_list():
     page = requests.get("https://www.wp.pl/")
     soup = BeautifulSoup(page.content, 'html.parser')
     news = soup.find_all(class_ = "lclzf3-0 egPcYF")
-    return [x.text for x in news[:4]]
+    return [x.text for x in news[:news_num]]
+
+def gazeta_list():
+    page = requests.get("http://wiadomosci.gazeta.pl/wiadomosci/0,0.html#s=NavLinks")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    news = soup.find_all(class_ = "article")
+    return [x.header.h2.text for x in news[:news_num]]
+
+def gazetapolska_list():
+    page = requests.get("https://www.gazetapolska.pl/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    news = soup.find_all(class_ = "item-list")
+    return [news[x].ul.li.text.replace("\n","") for x in range(news_num)]
 
 
 @app.route("/")
 @app.route("/home")
 def info():
-    feed_list = [onet_list(), wpolityce_list(), wp_list()]
+    feed_list = [onet_list(), wpolityce_list(), wp_list(), gazeta_list(), gazetapolska_list()]
     return render_template('home.html', feed_list=feed_list)
 
 

0ec0968c098f5d8238d84ff290ab951385f89192
Michal <siedlecki.michal@gmail.com>
2019-08-13 23:50:12 +0200

    link feature almost ready:)

--- app.py
+++ app.py
@@ -3,46 +3,80 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-news_num = 6
-
-
+news_num = 3
 class Feed:
-    def __init__(self, name, url, selector):
+    def __init__(self, name, url):
         self.name = name
         self.url = url
-        self.selector = selector
+        
 
-    def get_headers(self):
+    def getHeaders(self):
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content, 'html.parser')
-        news = soup.find_all(class_=self.selector)
 
-        if self.url == 'https://wiadomosci.onet.pl/':
-            return [x.next for x in news[1:(news_num - 1)]]
-        elif self.url == 'http://wiadomosci.gazeta.pl/wiadomosci/0,0.html#s=NavLinks':
-            return [x.header.h2.text for x in news[:news_num]]
-        elif self.url == 'https://www.gazetapolska.pl/':
-            return [news[x].ul.li.text.replace("\n", "") for x in range(news_num)]
-        else:
-            return [x.text for x in news[:news_num]]
+        
+        if self.name == 'Onet':
+            divs = soup.find_all(class_="sectionLine sectionLineMax")
+            articles_sets = [div.find_all('a') for div in divs]
+            onet_dic = {'name' : self.name}
+     
+            for articles in articles_sets:
+                for article in articles:
+                    if len(onet_dic) <= news_num+1:
+                        onet_dic[article.find(class_="title").text] = article.attrs.get('href')
+                    else:
+                        return onet_dic
+           
+        
+        
+        
+        if self.name == 'Wirtualna Polska':
+            divs = soup.find_all(class_="sc-1fu2hk8-0 jIlknD")
+            articles_sets = [div.find_all('a') for div in divs]
+            wp_dic = {'name' : self.name}         
+            for articles in articles_sets:
+                for article in articles:                
+                    if len(wp_dic) <= news_num+1:
+                        try:
+                            wp_dic[article.find_all('div')[1].text] = article.attrs.get('href')
+                        except IndexError:
+                            wp_dic[article.find('div').text] = article.attrs.get('href')
+                    else:
+                        return wp_dic
+                    
+        
+        if self.name == 'W Polityce':
+            divs = soup.find_all(class_="nu-lead-articles") + soup.find_all(class_="nu-tile-container nu-main-col--5")
+            articles_sets = [div.find_all('a') for div in divs]
+            wpolityce_dic = {'name' : self.name}
+            for articles in articles_sets:
+                for article in articles:
+                    if len(wpolityce_dic) <= news_num+1:
+                        wpolityce_dic[article.find(class_="short-title").text] = str(self.url[:-1])+str(article.attrs.get('href'))
+                    else:
+                        return wpolityce_dic
+
+        
+onet = Feed('Onet', 'https://www.onet.pl/')
+wp = Feed('Wirtualna Polska', 'https://www.wp.pl/')
+wpolityce = Feed('W Polityce', 'https://wpolityce.pl/')
+
+feed_dic_list = [onet, wp, wpolityce]
 
 
-onet = Feed('Onet', 'https://wiadomosci.onet.pl/', "driverItemTitle itemAnimation")
-wp = Feed('Wirtualna Polska', 'https://www.wp.pl/', "lclzf3-0 egPcYF")
-wpolityce = Feed('W Polityce', 'https://wpolityce.pl/', 'short-title')
-gazeta = Feed('Gazeta.pl', 'http://wiadomosci.gazeta.pl/wiadomosci/0,0.html#s=NavLinks', 'article')
-gazetapolska = Feed('Gazeta Polska', 'https://www.gazetapolska.pl/', 'item-list')
 
-feed_list = [onet, wp, wpolityce, gazeta, gazetapolska]
 
 
 @app.route("/")
 @app.route("/home")
 def info():
-    for feed in feed_list:
-        feed.get_headers()
+
+    titles_links = []
+    for feed in feed_dic_list:
+        titles_links.append(feed.getHeaders())    
+        
     
-    return render_template('home.html', feed_list=feed_list)
+    return render_template('home.html', titles_links=titles_links)
 
 
 if __name__ == '__main__':
---
+++ helper.py
@@ -0,0 +1,7 @@
+dic = { 
+    'osoba1' : 'michal',
+    'osoba2' : 'kasia',
+    'osoba3' : 'franek',
+    'osoba4' : 'klara'
+}
+print(len(dic))

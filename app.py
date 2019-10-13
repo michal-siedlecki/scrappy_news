d105f27a413d79529c77dd38207d428a224af155
Michal <siedlecki.michal@gmail.com>
2019-10-13 22:28:57 +0200

    full app v.1.0

--- app.py
+++ app.py
@@ -1,82 +1,85 @@
 import requests
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
+import pickle
 
+model = loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
+cv = pickle.load(open('count_vectorizer.sav', 'rb'))
 app = Flask(__name__)
-news_num = 3
+news_num = 5
+
+
+def getLabel(header):
+    header_cv = cv.transform([header])
+    return model.predict(header_cv)
 
 
 class Feed:
     def __init__(self, name, url):
         self.name = name
         self.url = url
-        
 
+    @property
     def getHeaders(self):
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content, 'html.parser')
 
-        
         if self.name == 'Onet':
             divs = soup.find_all(class_="sectionLine sectionLineMax")
             articles_sets = [div.find_all('a') for div in divs]
-            onet_dic = {'name' : self.name}
-     
+            onet_headers = []
             for articles in articles_sets:
                 for article in articles:
-                    if len(onet_dic) <= news_num+1:
-                        onet_dic[article.find(class_="title").text] = article.attrs.get('href')
+                    if len(onet_headers) <= news_num+1:
+                        header = article.find(class_="title").text
+                        link = article.attrs.get('href')
+                        label = getLabel(header)[0]
+                        onet_headers.append((header, link, label))
                     else:
-                        return onet_dic
-        
-        if self.name == 'Wirtualna Polska':
-            divs = soup.find_all(class_="sc-1fu2hk8-0 jIlknD")
-            articles_sets = [div.find_all('a') for div in divs]
-            wp_dic = {'name' : self.name}         
-            for articles in articles_sets:
-                for article in articles:                
-                    if len(wp_dic) <= news_num+1:
-                        try:
-                            wp_dic[article.find_all('div')[1].text] = article.attrs.get('href')
-                        except IndexError:
-                            wp_dic[article.find('div').text] = article.attrs.get('href')
-                    else:
-                        return wp_dic
+                        return onet_headers
                     
         if self.name == 'W Polityce':
             divs = soup.find_all(class_="nu-lead-articles") + soup.find_all(class_="nu-tile-container nu-main-col--5")
             articles_sets = [div.find_all('a') for div in divs]
-            wpolityce_dic = {'name' : self.name}
+            wpolityce_headers = []
             for articles in articles_sets:
                 for article in articles:
-                    if len(wpolityce_dic) <= news_num+1:
-                        wpolityce_dic[article.find(class_="short-title").text] = str(self.url[:-1])+str(article.attrs.get('href'))
+                    if len(wpolityce_headers) <= news_num+1:
+                        header = article.find(class_="short-title").text
+                        link = str(self.url[:-1])+str(article.attrs.get('href'))
+                        label = getLabel(header)[0]
+                        wpolityce_headers.append((header, link, label))
                     else:
-                        return wpolityce_dic
+                        return wpolityce_headers
+
+        if self.name == 'Wirtualna Polska':
+            wrapper = soup.find(class_="sc-1fu2hk8-0 ezOXAO")
+            divs = wrapper.find_all('div')
+            wp_headers = []
+            if len(wp_headers) <= news_num + 1:
+                for div in divs[8:20]:
+                    if div.a:
+                        header = div.text.strip()
+                        link = div.a.get('href')
+                        label = getLabel(header)[0]
+                        wp_headers.append((header, link, label))
+
+            wp_headers = list(set(wp_headers))
+            return wp_headers
 
         
 onet = Feed('Onet', 'https://www.onet.pl/')
 wp = Feed('Wirtualna Polska', 'https://www.wp.pl/')
 wpolityce = Feed('W Polityce', 'https://wpolityce.pl/')
 
-feed_list = [onet, wp, wpolityce]
-for feed in feed_list:
+@app.route("/")
+@app.route("/home")
+def info():
 
-    print(feed.getHeaders())
-
-# @app.route("/")
-# @app.route("/home")
-# def info():
-
-#     titles_links = []
-#     for feed in feed_list:
-#         titles_links.append(feed.getHeaders())    
-        
-    
-#     return render_template('home.html', titles_links=titles_links)
+    headers = onet.getHeaders + wpolityce.getHeaders + wp.getHeaders
+    return render_template('home.html', headers=headers)
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-#     app.run(debug=True)
-   
+    app.run(debug=True)
